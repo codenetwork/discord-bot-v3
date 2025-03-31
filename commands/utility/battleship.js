fadb8959e0fb5b76359d9dc00e38564fcf7d1a02
Sean Dewantoro <seanandrewvalentino@gmail.com>
2025-03-31 19:47:10 +1000

    Createcategory Command to Subcommand
    The createcategory command is now a subcommand of 'battlehsip'. Members
    can call it by doing /battleship createcategory

--- commands/utility/battleship.js
+++ commands/utility/battleship.js
@@ -1,37 +1,51 @@
 const { SlashCommandBuilder, ChannelType } = require('discord.js');
 
+// TODO:
+// 1. Create categories ✅
+// 2. Create roles and assign roles
+// 3. Get player (member) ids
+
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('createcategory')
+    .setName('battleship')
     .setDescription('Creates a new category given a name.')
-    .addStringOption((option) =>
-      option.setName('name').setDescription('Name of the category').setRequired(true)
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName('createcategory')
+        .setDescription('Creates a new category given a name.')
+        .addStringOption((option) =>
+          option.setName('name').setDescription('Name of the category to create').setRequired(true)
+        )
     ),
   async execute(interaction) {
-    const categoryName = interaction.options.getString('name');
+    const subcommand = interaction.options.getSubcommand();
 
-    const existingCategory = interaction.guild.channels.cache.find(
-      (channel) => channel.name === categoryName && channel.type === ChannelType.GuildCategory
-    );
+    if (subcommand === 'createcategory') {
+      const categoryName = interaction.options.getString('name');
 
-    if (existingCategory) {
-      return interaction.reply({
-        content: `Category "${categoryName}" already exists!`,
-        ephemeral: true,
-      });
-    }
+      const existingCategory = interaction.guild.channels.cache.find(
+        (channel) => channel.name === categoryName && channel.type === ChannelType.GuildCategory
+      );
 
-    try {
-      const newCategory = await interaction.guild.channels.create({
-        name: categoryName,
-        type: ChannelType.GuildCategory,
-      });
-      interaction.reply(`Category "${categoryName}" has been created!`);
-    } catch (error) {
-      interaction.reply({
-        content: `Failed to create category "${categoryName}" :(\n${error}`,
-        ephemeral: true,
-      });
+      if (existingCategory) {
+        return interaction.reply({
+          content: `Category "${categoryName}" already exists!`,
+          ephemeral: true,
+        });
+      }
+
+      try {
+        const newCategory = await interaction.guild.channels.create({
+          name: categoryName,
+          type: ChannelType.GuildCategory,
+        });
+        interaction.reply(`Category "${categoryName}" has been created!`);
+      } catch (error) {
+        interaction.reply({
+          content: `Failed to create category "${categoryName}" :(\n${error}`,
+          ephemeral: true,
+        });
+      }
     }
   },
 };

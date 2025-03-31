d3a07a9b966118d361bf8e84691dff3243c707cf
Sean Dewantoro <seanandrewvalentino@gmail.com>
2025-03-31 19:55:55 +1000

    Implement Subcommand deletecategory
    The deletecategory deletes a category given its name.

--- commands/utility/battleship.js
+++ commands/utility/battleship.js
@@ -8,13 +8,24 @@ const { SlashCommandBuilder, ChannelType } = require('discord.js');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('battleship')
-    .setDescription('Creates a new category given a name.')
+    .setDescription('Manage the battleship game')
     .addSubcommand((subcommand) =>
       subcommand
         .setName('createcategory')
         .setDescription('Creates a new category given a name.')
         .addStringOption((option) =>
           option.setName('name').setDescription('Name of the category to create').setRequired(true)
+        )
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName('deletecategory')
+        .setDescription('Deletes a category')
+        .addStringOption((option) =>
+          option
+            .setName('name')
+            .setDescription('Name of the category to be deleted.')
+            .setRequired(true)
         )
     ),
   async execute(interaction) {
@@ -43,6 +54,29 @@ module.exports = {
       } catch (error) {
         interaction.reply({
           content: `Failed to create category "${categoryName}" :(\n${error}`,
+          ephemeral: true,
+        });
+      }
+    } else if (subcommand === 'deletecategory') {
+      const categoryName = interaction.options.getString('name');
+
+      const existingCategory = interaction.guild.channels.cache.find(
+        (channel) => channel.name === categoryName && channel.type === ChannelType.GuildCategory
+      );
+
+      if (!existingCategory) {
+        return interaction.reply({
+          content: `Category "${categoryName}" does not exist!`,
+          ephemeral: true,
+        });
+      }
+
+      try {
+        await existingCategory.delete();
+        return interaction.reply(`Category "${categoryName}" has been deleted!`);
+      } catch (error) {
+        return interaction.reply({
+          content: `Failed to delete "${categoryName}" :(`,
           ephemeral: true,
         });
       }

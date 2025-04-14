6971d5cfec239ac3483af5b432c15447a2993300
ArcingFirehawk <anthony.choi204@gmail.com>
2025-04-14 12:14:49 +1000

    Added documentation to some commands.

--- commands/utilities/map.js
+++ commands/utilities/map.js
@@ -1,9 +1,13 @@
-// Discord slash command that links to a QUT's campus maps.
+/**
+ * @name: map.js
+ * @description: Discord slash command that links to QUT's campus maps.
+ * @author: Anthony Choi. Assistance provided by William Qu and Yiming He.
+ */
 
 
 
 // VARAIABLES
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, MessageFlags } = require("discord.js");
 const link = "https://www.qut.edu.au/about/campuses-and-facilities/maps-and-getting-here";
 let response;
 
@@ -14,17 +18,16 @@ module.exports =
 	// Sets up the command.
 	data: new SlashCommandBuilder()
 		.setName("map")
-		.setDescription("DMs the user a link to the map of the selected QUT campus."),
+		.setDescription("Returns the link to QUT's campus maps."),
 
 	// Enables the command's functionality.
 	async execute(interaction)
 	{
-		
 		response = `Here's the link to the campuses' maps: ${link}.`;
 		await interaction.reply(
 		{
 			content: response,
-			ephemeral: true
-		}) 
+			flags: MessageFlags.Ephemeral
+		})
 	},
 };

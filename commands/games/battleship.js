c458838e6222975bb789e95ad30bbd551cf94789
Sean Dewantoro <seanandrewvalentino@gmail.com>
2025-04-19 23:47:10 +1000

    Implement Invite Subcommand
    Implemented sending a private message from a bot to the invitee
    initiated by the inviter.

--- commands/games/battleship.js
+++ commands/games/battleship.js
@@ -1,10 +1,15 @@
-const { SlashCommandBuilder, ChannelType, PermissionOverwrites } = require('discord.js');
+const {
+  SlashCommandBuilder,
+  ChannelType,
+  PermissionOverwrites,
+  MessageFlags,
+} = require('discord.js');
 
 // TODO:
 // 1. Create categories ✅
 // 2. Delete categories ✅
 // 3. Create text channels ✅
-// 4. Send private DMs to members
+// 4. Send private DMs to members ✅
 // 5. Send private DMs with buttons to members
 // 6. Members reply to private DMs
 // 7. Create session once p2 accepts (includes validation of time, etc.)
@@ -19,14 +24,14 @@ const { SlashCommandBuilder, ChannelType, PermissionOverwrites } = require('disc
  *       "id": Member.id,
  *       "board": [n x m],
  *       "guesses": [n x m],
- *       "textChannelId": Channel.i
+ *       "textChannelId": Channel.id
  *     },
  *    p2:
  *     {
  *       "id": Member.id,
  *       "board": [n x m],
  *       "guesses": [n x m],
- *       "textChannelId": Channel.i
+ *       "textChannelId": Channel.id
  *     },
  *    inviteTimestamp: Date,
  *    inviteAcceptedTimestamp: Date
@@ -67,6 +72,14 @@ module.exports = {
             .setDescription('Name of the text channel to be created.')
             .setRequired(true)
         )
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName('invite')
+        .setDescription('Invites another player to play')
+        .addUserOption((option) =>
+          option.setName('player').setDescription('The player you want to invite').setRequired(true)
+        )
     ),
   async execute(interaction) {
     const subcommand = interaction.options.getSubcommand();
@@ -81,7 +94,7 @@ module.exports = {
       if (existingCategory) {
         return interaction.reply({
           content: `Category "${categoryName}" already exists!`,
-          ephemeral: true,
+          ephemeral: MessageFlags.Ephemeral,
         });
       }
 
@@ -94,7 +107,7 @@ module.exports = {
       } catch (error) {
         interaction.reply({
           content: `Failed to create category "${categoryName}" :(\n${error}`,
-          ephemeral: true,
+          ephemeral: MessageFlags.Ephemeral,
         });
       }
     } else if (subcommand === 'deletecategory') {
@@ -107,7 +120,7 @@ module.exports = {
       if (!existingCategory) {
         return interaction.reply({
           content: `Category "${categoryName}" does not exist!`,
-          ephemeral: true,
+          ephemeral: MessageFlags.Ephemeral,
         });
       }
 
@@ -117,7 +130,7 @@ module.exports = {
       } catch (error) {
         return interaction.reply({
           content: `Failed to delete "${categoryName}" :(`,
-          ephemeral: true,
+          ephemeral: MessageFlags.Ephemeral,
         });
       }
     } else if (subcommand === 'createtextchannel') {
@@ -128,7 +141,7 @@ module.exports = {
       if (!battleshipCategory) {
         return interaction.reply({
           content: `Category "Battleship" not found. Please create this category first.`,
-          ephemeral: true,
+          ephemeral: MessageFlags.Ephemeral,
         });
       }
 
@@ -145,6 +158,31 @@ module.exports = {
         });
         return interaction.reply(`Text channel "${newChannel.name}" has been created!`);
       } catch (error) {}
+    } else if (subcommand === 'invite') {
+      const inviter = interaction.user;
+      const invitee = interaction.options.getUser('player');
+
+      // In case invitee is invalid
+      if (!invitee || inviter.id == invitee.id) {
+        return interaction.reply({
+          content: 'You must invite a valid player!',
+          ephemeral: MessageFlags.Ephemeral,
+        });
+      }
+
+      try {
+        await invitee.send(`Ma nigga you've been invited by ${inviter.username}!`);
+        return interaction.reply({
+          content: `Invitation sent to ${invitee.username}.`,
+          ephemeral: MessageFlags.Ephemeral,
+        });
+      } catch (error) {
+        console.error('Failed to send DM: ', error);
+        return interaction.reply({
+          content: `Could not send a DM to ${invitee.username} They may have DMs disabled.`,
+          ephemeral: MessageFlags.Ephemeral,
+        });
+      }
     }
   },
 };
